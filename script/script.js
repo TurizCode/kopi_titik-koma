b771830d5ab4101d70788ead8902440bd51e7752
TurizCode <turizcode@gmail.com>
2023-11-28 01:29:55 +0800

    adding a bunch new stuff and feature and fixing

--- script/script.js
+++ script/script.js
@@ -1,9 +1,35 @@
+// NOTE: Maaf bahasanya campur-campur :)
 const navbarNav = document.querySelector(".navbar-nav");
 const hamburgerMenu = document.querySelector("#hamburger-menu");
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
 const searchButton = document.querySelector("#search-button");
+const shoppingCartButton = document.querySelector("#shopping-cart-button");
+const shoppingCart = document.querySelector(".shopping-cart");
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemdetailButton = document.querySelector(".item-detail-button");
+const itemdetailButtons = document.querySelectorAll(".item-detail-button");
+const closeItemDetailButton = document.querySelector(".close-icon");
+const modalContainer = document.querySelector(".modal-container");
 
+// HAMBURGER MENU
+// Toggle class active ketika search button diklik
+document.querySelector("#hamburger-menu").onclick = (e) => {
+  navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+// klik di luar search bar untuk menghilangkan search form
+document.addEventListener("click", (event) => {
+  // Check if the clicked element is outside the search bar
+  if (
+    !searchButton.contains(event.target) &&
+    !searchForm.contains(event.target)
+  ) {
+    searchForm.classList.remove("active");
+  }
+});
+
+// SEARCH FORM
 // Toggle class active ketika search form diklik
 document.querySelector("#search-button").onclick = (e) => {
   searchForm.classList.toggle("active");
@@ -21,18 +47,62 @@ document.addEventListener("click", (event) => {
   }
 });
 
-// Toggle class active ketika search button diklik
-document.querySelector("#hamburger-menu").onclick = (e) => {
-  navbarNav.classList.toggle("active");
+// SHOPPING CART
+// Toggle class active ketika shopping cart button diklik
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
   e.preventDefault();
 };
-// klik di luar sidebar untuk menghilangkan search form
+// klik di luar shopping cart button untuk menghilangkan shopping cart section
 document.addEventListener("click", (event) => {
-  // Check if the clicked element is outside the sidebar
+  // Check if the clicked element is outside the shoppingcart
   if (
-    !searchButton.contains(event.target) &&
-    !searchForm.contains(event.target)
+    !shoppingCartButton.contains(event.target) &&
+    !shoppingCart.contains(event.target)
   ) {
-    searchForm.classList.remove("active");
+    shoppingCart.classList.remove("active");
   }
 });
+
+// MODAL BOX
+// change the display of the modal box/ item detail model from none to flex
+itemdetailButton.onclick = (e) => {
+  modalContainer.classList.remove("out");
+  itemDetailModal.style.display = "flex";
+  e.preventDefault();
+};
+// change the display of the modal box/ item detail model from flex to none
+closeItemDetailButton.onclick = (e) => {
+  modalContainer.classList.add("out");
+  setTimeout(() => {
+    itemDetailModal.style.display = "none";
+  }, 500);
+  e.preventDefault();
+};
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    modalContainer.classList.add("out");
+    setTimeout(() => {
+      itemDetailModal.style.display = "none";
+    }, 500);
+  }
+};
+
+itemdetailButtons.forEach((button) => {
+  button.onclick = (e) => {
+    modalContainer.classList.remove("out");
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// TODO: Add a active class on the shown modal container with a unique id so it can provide a syncronized details based on the product id
+
+// Add an active class to the shown modal container with a unique id (perhaps you can put the unique id on each of the eye icon)
+// itemDetailModal.classList.add("active");
+// itemDetailModal.id = "uniqueId";
+
+// Provide synchronized details based on the product id
+// function syncDetails(productId) {
+//   Your code here to fetch and display details based on the productId
+// }
