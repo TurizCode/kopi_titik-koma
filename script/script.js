18837e635b48ac31f3da0af2290bd68b971765ac
TurizCode <turizcode@gmail.com>
2023-11-27 10:38:49 +0800

    fixing navbar menu and adding search bar

--- script/script.js
+++ script/script.js
@@ -1,14 +1,16 @@
 const navbarNav = document.querySelector(".navbar-nav");
 const hamburgerMenu = document.querySelector("#hamburger-menu");
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const searchButton = document.querySelector("#search-button");
 
-// Toggle class active ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
+// Toggle class active ketika search form diklik
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
 };
-
-// klik di luar sidebar untuk menghilangkan nav
-
-// Hide navigation when clicking outside sidebar
+// klik di luar sidebar untuk menghilangkan navbar
 document.addEventListener("click", (event) => {
   // Check if the clicked element is outside the sidebar
   if (
@@ -16,5 +18,21 @@ document.addEventListener("click", (event) => {
     !hamburgerMenu.contains(event.target)
   ) {
     navbarNav.classList.remove("active");
+  }
+});
+
+// Toggle class active ketika search button diklik
+document.querySelector("#hamburger-menu").onclick = (e) => {
+  navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+// klik di luar sidebar untuk menghilangkan search form
+document.addEventListener("click", (event) => {
+  // Check if the clicked element is outside the sidebar
+  if (
+    !searchButton.contains(event.target) &&
+    !searchForm.contains(event.target)
+  ) {
+    searchForm.classList.remove("active");
   }
 });
